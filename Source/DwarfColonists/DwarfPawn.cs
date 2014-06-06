10b71a6b2eec7efe5eac53d9c734d3caa4d42d9e
Mispy <^_^@mispy.me>
2014-06-07 06:09:03 +1000

    Initial commit!

---
+++ Source/DwarfColonists/DwarfPawn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using Verse.AI;
+
+namespace Verse
+{
+    internal class DwarfPawn : Pawn
+    {
+        public DwarfPawn() : base() {
+        }
+
+        public static Graphic_Apparel DwarfGraphic = new Graphic_Apparel("Things/Pawn/Dwarf/Dwarf", new Color(1, 1, 1));
+        public static Graphic_Apparel DwarfMilitiaGraphic = new Graphic_Apparel("Things/Pawn/Dwarf/Dwarf_Militia", new Color(1, 1, 1));
+
+        public override void Tick()
+        {
+            if (this.IsColonist && this.drawer != null && this.drawer.renderer != null && this.drawer.renderer.graphics != null)
+            {
+                this.drawer.renderer.graphics.headGraphic = null;
+                this.apparel = null;
+
+                if (this.playerController.Drafted) {
+                    this.drawer.renderer.graphics.nakedGraphic = DwarfMilitiaGraphic;
+                } else {
+                    this.drawer.renderer.graphics.nakedGraphic = DwarfGraphic;
+                }
+            }
+
+            base.Tick();
+        }
+    }
+}
